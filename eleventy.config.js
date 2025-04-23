6ea3b3f2868f6685ee35a63b088fbbaac4bf52c9
JYheong <jamtengleep@naver.com>
2025-04-23 22:38:33 +1000

    11ty

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,37 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
+
+
+		// Copy the contents of the `public` folder to the output folder
+	// For example, `./public/css/` ends up in `_site/css/`
+	eleventyConfig
+		.addPassthroughCopy({
+			"./public/": "/"
+		})
+		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
+
+	// Run Eleventy when these files change:
+	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
+
+	// Watch CSS files
+	// eleventyConfig.addWatchTarget("./css/**/*.css");
+
+
+	eleventyConfig.addPassthroughCopy("./styles/") // https://robmc.dev/blog/add-css-to-your-eleventy-site/
+	eleventyConfig.addWatchTarget("./styles/");
+
+	// Watch images for the image pipeline.
+	eleventyConfig.addWatchTarget("content/**/*.{svg,webp,png,jpg,jpeg,gif}");
+
+
+
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+
+	return {
+		dir: {
+			input: ".",
+		}
+	}
 };
