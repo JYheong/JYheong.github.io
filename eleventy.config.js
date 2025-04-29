a750d6de9d723bc1cff42fb9622ab003fcb891fb
ju yeong <jamtengleep@naver.com>
2025-04-29 22:43:01 +0900

    css 주소수정

--- eleventy.config.js
+++ eleventy.config.js
@@ -32,6 +32,9 @@ module.exports = function (eleventyConfig) {
 	return {
 		dir: {
 			input: ".",
+			includes: "_includes",
+			data: "_data",
+			output: "_site"
 		},
 		pathPrefix: "/JYheong.github.io/"
 	}
